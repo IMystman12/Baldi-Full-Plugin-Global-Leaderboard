bb7d8f874030c83e6b2ee3ad001e1b80f389142b
BDC <BDC@IMYSTMAN12>
2025-11-10 21:05:59 +0800

    添加项目文件。

---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using BepInEx;
+using UnityEngine;
+
+[BepInPlugin("baldifull.template", "Baldi Full Template", "1.0")]
+public class BasePlugin : BaseUnityPlugin
+{
+    public void Awake()
+    {
+        new Harmony("baldifull.template").PatchAll();
+        Debug.Log("Awaking!");
+    }
+}
+[HarmonyPatch]
+public static class Patch
+{
+    [HarmonyPatch(typeof(NameManager), "Awake"), HarmonyPostfix]
+    public static void Postfix()
+    {
+        Debug.Log("Load your assets or modify base game here!");
+    }
+}
